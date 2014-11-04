c8a6984a6f6bb6d3ec77a780a3cfb123c0bfb165
omidp <omidpourhadi@gmail.com>
2014-11-04 19:37:55 +0330

    add gauge component

---
+++ gauge/src/main/java/ir/sapp/component/UIGauge.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+package ir.sapp.component;
+
+import javax.faces.component.UIComponentBase;
+
+/**
+ * JSF component class
+ *
+ */
+public abstract class UIGauge extends UIComponentBase {
+	
+	public static final String COMPONENT_TYPE = "ir.sapp.Gauge";
+	
+	public static final String COMPONENT_FAMILY = "ir.sapp.Gauge";
+	
+}
---
+++ gauge/src/main/java/ir/sapp/renderkit/GaugeRendererBase.java
@@ -0,0 +1,14 @@
+package ir.sapp.renderkit;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import org.ajax4jsf.renderkit.HeaderResourcesRendererBase;
+
+public abstract class GaugeRendererBase extends HeaderResourcesRendererBase{
+
+
+
+	
+	
+}
