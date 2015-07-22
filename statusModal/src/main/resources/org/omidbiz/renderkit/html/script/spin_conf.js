6dd8a89d5a9e3965d9c1af9e0807cf7cd4fb8db0
Omidp <pourhadi@fanpardaz.com>
2015-07-22 16:18:21 +0430

    change loading and tipsy direction rtl

--- statusModal/src/main/resources/org/omidbiz/renderkit/html/script/spin_conf.js
+++ statusModal/src/main/resources/org/omidbiz/renderkit/html/script/spin_conf.js
@@ -1,18 +1,22 @@
 var opts = {
-	lines: 15, // The number of lines to draw
-	length: 5, // The length of each line
-	width: 5, // The line thickness
-	radius: 20, // The radius of the inner circle
-	corners: 1, // Corner roundness (0..1)
-	rotate: 0, // The rotation offset
-	direction: 1, // 1: clockwise, -1: counterclockwise
-	color: '#000', // #rgb or #rrggbb or array of colors
-	speed: 1, // Rounds per second
-	trail: 50, // Afterglow percentage
-	shadow: false, // Whether to render a shadow
-	hwaccel: false, // Whether to use hardware acceleration
-	className: 'spinner', // The CSS class to assign to the spinner
-	zIndex: 2e9, // The z-index (defaults to 2000000000)
-	top: '50%', // Top position relative to parent
-	left: '50%' // Left position relative to parent
+	lines: 15 // The number of lines to draw
+, length: 0 // The length of each line
+, width: 23 // The line thickness
+, radius: 62 // The radius of the inner circle
+, scale: 1.75 // Scales overall size of the spinner
+, corners: 0.4 // Corner roundness (0..1)
+, color: '#000' // #rgb or #rrggbb or array of colors
+, opacity: 0.05 // Opacity of the lines
+, rotate: 0 // The rotation offset
+, direction: 1 // 1: clockwise, -1: counterclockwise
+, speed: 1.3 // Rounds per second
+, trail: 70 // Afterglow percentage
+, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+, zIndex: 2e9 // The z-index (defaults to 2000000000)
+, className: 'spinner' // The CSS class to assign to the spinner
+, top: '50%' // Top position relative to parent
+, left: '50%' // Left position relative to parent
+, shadow: false // Whether to render a shadow
+, hwaccel: false // Whether to use hardware acceleration
+, position: 'absolute' // Element positioning
 };
