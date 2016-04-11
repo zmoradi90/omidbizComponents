e0ab7787e08362401b781ae50d205a68f3df8da6
SHK <shahrad.kashany@gmail.com>
2016-04-11 11:09:45 +0430

    add new component progress bar

---
+++ progressbar/src/main/java/org/omidbiz/component/UIProgressbar.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+package org.omidbiz.component;
+
+import javax.faces.component.UIComponentBase;
+
+/**
+ * JSF component class
+ *
+ */
+public abstract class UIProgressbar extends UIComponentBase {
+	
+	public static final String COMPONENT_TYPE = "org.omidbiz.Progressbar";
+	
+	public static final String COMPONENT_FAMILY = "org.omidbiz.Progressbar";
+	
+}
